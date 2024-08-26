fb84989aa79e621d197c768b2a9005f61add12c5
Bilal <bilalbelkebir9@gmail.com>
2024-08-26 15:49:21 +0100

    make the Meme get the urls from the meme data js file

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import Data from "../MemeData"
 import "./Meme.css";
 
+
+
 function Meme() {
+    const [url , setUrl] = React.useState('')
+    function handleClick() {
+        const random = Math.floor(Math.random() * Data.data.memes.length)
+        setUrl(Data.data.memes[random].url)
+    }
     return (
         <div className='container'>
             <div className='cont'>
                 <div>
-                    <label for="">top text</label><br />
+                    <label htmlFor="top-text">top text</label><br />
                     <input type='text' id="top-text" />
                 </div>
                 <div>
-                    <label for="bottom-text">bottom text</label><br />
+                    <label htmlFor="bottom-text">bottom text</label><br />
                     <input type='text' id='bottom-text' />
                 </div>
             </div>
-            <button id="button">git a new meme image</button>
+            <button onClick={handleClick} id="button">git a new meme image</button>
+            <div className='img-container'>
+                <img src={url} alt="meme" />
+            </div>
         </div>
     )
 }
